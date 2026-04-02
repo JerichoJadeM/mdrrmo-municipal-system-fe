ab9543fe97a3aaec08764841fbde9a13028c279a
Jericho Jade Madolid <saserdote.jerichojade@gmail.com>
2026-04-03 01:21:33 +0800

    config: change url to railway

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   //API_BASE: "http://localhost:8080/api"
   //API_BASE: "https://mdrrmo-municipal-system-be.onrender.com/api"
-  API_BASE: "hmdrrmo-municipal-system-be-production.up.railway.app/api"
+  API_BASE: "https://hmdrrmo-municipal-system-be-production.up.railway.app/api"
 };
 
 var API_BASE = window.APP_CONFIG.API_BASE;
